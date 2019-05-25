8bf7bd96b9fcf38ad596002f941002f32264df8d
Teemu <karppinenteemu@outlook.com>
2019-05-25 23:12:33 +0300

    Remove unwanted build directory

--- build/src/init.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var boksi_conf_json_1 = __importDefault(require("../boksi-conf.json"));
-var Boksi_1 = __importDefault(require("./Boksi"));
-var boksi = new Boksi_1.default(boksi_conf_json_1.default);
